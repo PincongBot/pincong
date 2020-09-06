068dbba5c45b8500d7dfcedb585e1458ce2bc5ae
PincongBot <PincongBot@pm.me>
2020-09-05 22:20:01 -0400

    code

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const Backup = require("wecenter-backup-helper").default
+const process = require("process")
+
+const options = {
+    siteUrl: process.env.SITE_URL,
+    userAgent: process.env.UA,
+    destDir: process.env.FILES_DIR,
+    concurrency: +process.env.CONCURRENCY,
+    progress(item) {
+        if (!item.parentType) { // root item
+            console.log("📝", item.type, item.id)
+        }
+    }
+}
+
+const startId = +process.env.START_ID || undefined
+const endId = +process.env.END_ID || undefined
+
+Backup(process.env.BACKUP_TYPE, options, startId, endId)
